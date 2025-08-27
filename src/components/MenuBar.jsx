23f153f86bfd4efd3de727a879a2120d5f315e93
Djedje SEWOVON <jesssewovon@gmail.com>
2025-08-27 18:54:21 +0000

    Updates

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,7 +3,35 @@ import { NavLink, Link } from 'react-router';
 export default function MenuBar() {
     return (
     	<>
-	    	<h1>MenuBar</h1>
+	    	<div className="menubar-area footer-fixed">
+				<div className="toolbar-inner menubar-nav">
+					<NavLink to="/" className={({ isActive }) =>
+			            isActive ? "nav-link active" : "nav-link"
+			        }>
+						<i className="flaticon flaticon-dog-house"></i>
+					</NavLink>
+					<NavLink to="explore" className={({ isActive }) =>
+			            isActive ? "nav-link active" : "nav-link"
+			        }>
+						<i className="flaticon flaticon-search"></i>
+					</NavLink>
+					<NavLink to="wishlist" className={({ isActive }) =>
+			            isActive ? "nav-link active" : "nav-link"
+			        }>
+						<i className="flaticon flaticon-heart"></i>
+					</NavLink>
+					<NavLink to="chat-list" className={({ isActive }) =>
+			            isActive ? "nav-link active" : "nav-link"
+			        }>
+						<i className="flaticon flaticon-chat-1"></i>
+					</NavLink>
+					<NavLink to="profile" className={({ isActive }) =>
+			            isActive ? "nav-link active" : "nav-link"
+			        }>
+						<i className="flaticon flaticon-user"></i>
+					</NavLink>
+				</div>
+			</div>
 		</>
     );
 }
