aac86c54baa6c2d758d8d689701a4025f8923fca
Djedje SEWOVON <jesssewovon@gmail.com>
2025-08-30 23:43:18 +0000

    Updates

--- src/pages/Registration/Gender.jsx
+++ src/pages/Registration/Gender.jsx
@@ -32,13 +32,66 @@ export default function Gender() {
                         <div className="section-head ps-0">
                             <h3>What's your gender ?</h3>
                         </div>
-                        
+                        <div className="radio style-2">
+                            <label className="radio-label" htmlFor="women">
+                                <input type="radio" name="radio2" value="women"
+                                    id="women" 
+                                    checked={
+                                        profileForm.gender ===
+                                        "women"
+                                    }
+                                    onChange={() =>
+                                        handleRadioChange(
+                                            "women"
+                                        )
+                                    }/>
+                                <span className="checkmark">						
+                                    <span className="text">Women</span>
+                                    <span className="check"></span>							
+                                </span>
+                            </label>
+                            <label className="radio-label" htmlFor="men">
+                                <input
+                                    type="radio" name="radio2" value="men"
+                                    id="men"
+                                    checked={
+                                        profileForm.gender ===
+                                        "men"
+                                    }
+                                    onChange={() =>
+                                        handleRadioChange(
+                                            "men"
+                                        )
+                                    }/>
+                                <span className="checkmark">
+                                    <span className="text">Men</span>
+                                    <span className="check"></span>							
+                                </span>
+                            </label>
+                            <label className="radio-label" htmlFor="other">
+                                <input type="radio" name="radio2" value="other"
+                                    id="other"
+                                    checked={
+                                        profileForm.gender ===
+                                        "other"
+                                    }
+                                    onChange={() =>
+                                        handleRadioChange(
+                                            "other"
+                                        )
+                                    }/>
+                                <span className="checkmark">
+                                    <span className="text">Other</span>	
+                                    <span className="check"></span>							
+                                </span>
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="footer fixed bg-white">
                 <div className="container">
-                    <button disabled={profileForm.gender==""} onClick={() => navigate('/registration/sexual-orientation')} className="btn btn-lg btn-gradient w-100 dz-flex-box btn-shadow rounded-xl">Next</button>
+                    <button disabled={profileForm?.gender==""} onClick={() => navigate('/registration/sexual-orientation')} className="btn btn-lg btn-gradient w-100 dz-flex-box btn-shadow rounded-xl">Next</button>
                 </div>
             </div>
         </>
