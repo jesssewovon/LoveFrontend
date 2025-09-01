ffc72831b41a3eaee50438847445ce6de714bf0b
Djedje SEWOVON <jesssewovon@gmail.com>
2025-09-01 23:48:03 +0000

    Updates

--- src/store/userSlice.jsx
+++ src/store/userSlice.jsx
@@ -73,9 +73,9 @@ const userSlice = createSlice({
         if (data.status==="success") {
             state.isLoggedIn = true;
             state.token = data.token;
-            if (data.current_user_for_automatic_update) {
+            /* if (data.current_user_for_automatic_update) {
               state.user = data?.current_user_for_automatic_update??null;
-            }
+            } */
             /* MySwal.fire({
               title: "Hello!",
               text: "Connexion new1",
