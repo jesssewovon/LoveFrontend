3e4a2f85c85e497facf6e016a6ae59ecb1d7bb4f
Djedje SEWOVON <jesssewovon@gmail.com>
2025-09-18 10:46:58 +0000

    Updates

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -125,43 +125,11 @@ export default function Landing() {
                                 </div>
                             </SwiperSlide>
                         </Swiper>
-                        {/* <div className="swiper get-started">
-                            <div className="swiper-wrapper">
-                                <div className="swiper-slide">
-                                    <div className="slide-info">
-                                        <div className="started">
-                                            <h2 className="title mb-3">Start Your Dating Story</h2>
-                                            <p>Your companion for meaningful connections</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide">
-                                    <div className="slide-info">
-                                        <div className="started">
-                                            <h2 className="title mb-3">Begin Your Chapter of Love</h2>
-                                            <p>Embrace the dating world armed with tools and support</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide">
-                                    <div className="slide-info">
-                                        <div className="started">
-                                            <h2 className="title mb-3">Your Journey of Connection</h2>
-                                            <p>Explore essentials and delights that boost confidence</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="swiper-btn">
-                                <div className="swiper-pagination style-1 flex-1"></div>
-                            </div>
-                        </div> */}
                     </div>
                     <div className="bottom-btn container">
                         <button disabled={isSaving} onClick={() => findSomeone()} className="btn btn-lg btn-gradient w-100 dz-flex-box btn-shadow rounded-xl">
-                            Find Someone {t('log_in')}<Loader/>
+                            {t('Find Someone')}<Loader/>
                         </button>
-                        {/* <button disabled={isLoading} onClick={() => navigate('/registration-firstname')} className="btn btn-lg btn-gradient w-100 dz-flex-box btn-shadow rounded-xl">Find Someone<Loader/></button> */}
                     </div>
                 </div>
             </div>
