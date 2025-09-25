7f27c32b3754cc4effca2c2a21bbaab8bfbbcbf6
Djedje SEWOVON <jesssewovon@gmail.com>
2025-09-25 19:54:30 +0000

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Settings from './pages/Settings';
 import Filter from './pages/Filter';
 import ProfileDetails from './pages/ProfileDetails';
 import Donation from './pages/Donation';
+import Subscription from './pages/Subscription';
 
 import FirstName from './pages/Registration/FirstName';
 import BirthDate from './pages/Registration/BirthDate';
@@ -105,6 +106,7 @@ function App() {
               <Route path="/filter" element={<Filter />} />
               <Route path="/profile-details/:id" element={<ProfileDetails />} />
               <Route path="/donation" element={<Donation />} />
+              <Route path="/subscription" element={<Subscription />} />
               <Route path="/registration-firstname" element={<FirstName />} />
               <Route path="/registration-birth-date" element={<BirthDate />} />
               <Route path="/registration-gender" element={<Gender />} />
---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,305 @@
+import { Link } from 'react-router';
+
+import Header from '../components/Header';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState, useRef, useEffect, useCallback } from "react";
+import { useTranslation } from 'react-i18next';
+
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+import { setIsLoading, setIsSaving, setDateFilter,
+  setUser, setReloadHomePage, changeLanguage, 
+} from "../store/userSlice";
+//import { navigate } from "../navigationService";
+import { useNavigate } from 'react-router';
+import api from "../api";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function Filter() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const {t} = useTranslation()
+  const { isLoggedIn, isLoading, user, dateFilter } = useSelector((state) => state.user);
+
+  const swiperRef = useRef();
+  const [activeSubscription, setActiveSubscription] = useState(0);
+
+  // Get my profile from API
+  const getSubscriptions = async () => {
+      dispatch(setIsLoading(true));
+      try {
+          const res = await api.get(`get-my-profile`);
+          console.log(`get-my-profile`, res.data); // adjust to your API structure
+          
+      } catch (err) {
+          console.error("Error fetching users:", err);
+      }
+      dispatch(setIsLoading(false));
+  };
+
+  // Load data on page change
+  useEffect(() => {
+    getSubscriptions();
+    console.log('swiperRef', swiperRef)
+    //dispatch(changeLanguage('fr'))
+  }, []);
+
+  if (isLoading) {
+      return (
+          <>
+              <Header showBackButton={true} title={"Edit profile"} showWishList={false}/>
+              <div className="page-content space-top p-b65">
+                  <div className="container fixed-full-area">
+                      <div className="flex items-center justify-center h-screen bg-gray-100">
+                          <div className="" style={{width: "100%", height: "70vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                              <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </>
+      );
+  }
+
+  return (
+    <>
+      <Header showBackButton={true} title={"My subscription"} showWishList={false}/>
+      <div class="page-content space-top p-b60">
+        <div class="container"> 
+          <div class="dz-subscribe-area">
+            <div class="subscribe-area-top">
+              <div class="swiper subscription-swiper2">
+                <Swiper
+                    ref={swiperRef}
+                    onSlideChange={(swiper) => {
+                      //alert(swiper.activeIndex)
+                      setActiveSubscription(swiper.activeIndex)
+                    }}
+                    spaceBetween={30}
+                    speed={1500}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: true,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={false}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper get-started"
+                >
+                    <SwiperSlide>
+                        <div class="subscribe-box plus">
+                          <h3 class="title">W3Dating App</h3>
+                          <div class="badge">PLUS</div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div class="subscribe-box gold">
+                          <h3 class="title">W3Dating App</h3>
+                          <div class="badge">GOLD</div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div class="subscribe-box platinum">
+                          <h3 class="title">W3Dating App</h3>
+                          <div class="badge">PLATINUM</div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+                <div class="swiper-btn">
+                  <div class="swiper-pagination style-1 flex-1"></div>
+                </div>
+              </div>
+            </div>
+            {activeSubscription ===0 && (<div class="subscribe-content plus">
+              <ul class="pricing-data">
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Likes</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>See Who Likes You</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>Priority Likes</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Rewinds</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>1 Free Boost per month</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>5 Free Super Likes per week</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>Message Before Matching</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Passport</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>Top Picks</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Your Profile</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who Sees You</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who You See</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Hide Ads</span>
+                </li>
+              </ul>
+              <div class="bottom-btn container bg-white text-center px-5">
+                <a href="javascript:void(0);" class="btn btn-gradient dz-flex-box btn-shadow rounded-xl">Starting at <span class="ms-2"><i class="fa-solid fa-indian-rupee-sign"></i></span> 450</a>
+              </div>
+            </div>)}
+            {activeSubscription ===1 && (<div class="subscribe-content gold">
+              <ul class="pricing-data">
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Likes</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>See Who Likes You</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>Priority Likes</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Rewinds</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>1 Free Boost per month</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>5 Free Super Likes per week</span>
+                </li>
+                <li class="list-false">
+                  <i class="icon feather icon-lock"></i>
+                  <span>Message Before Matching</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Passport</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Top Picks</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Your Profile</span>
+                </li> 
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who Sees You</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who You See</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Hide Ads</span>
+                </li>
+              </ul>
+              <div class="bottom-btn container bg-white text-center px-5">
+                <a href="javascript:void(0);" class="btn btn-gradient dz-flex-box btn-shadow rounded-xl">Starting at <span class="ms-2"><i class="fa-solid fa-indian-rupee-sign"></i></span> 650</a>
+              </div>
+            </div>)}
+            {activeSubscription ===2 && (<div class="subscribe-content platinum">
+              <ul class="pricing-data">
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Likes</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>See Who Likes You</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Priority Likes</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Unlimited Rewinds</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>1 Free Boost per month</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>5 Free Super Likes per week</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Message Before Matching</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Passport</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Top Picks</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Your Profile</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who Sees You</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Control Who You See</span>
+                </li>
+                <li class="list-true">
+                  <i class="icon feather icon-check"></i>
+                  <span>Hide Ads</span>
+                </li>
+              </ul>
+              <div class="bottom-btn container bg-white text-center px-5">
+                <a href="javascript:void(0);" class="btn btn-gradient dz-flex-box btn-shadow rounded-xl">Starting at <span class="ms-2"><i class="fa-solid fa-indian-rupee-sign"></i></span> 999</a>
+              </div>
+            </div>)}
+          </div>
+        </div> 
+      </div>
+    </>
+  );
+}
