5a9aa0ac96d333396147fadf957b9ab33d3b71ac
Djedje SEWOVON <jesssewovon@gmail.com>
2025-10-04 14:00:22 +0000

    Updates

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -19,6 +19,8 @@ export default function WishList({ savedScroll, onSaveScroll }) {
   const { isLoading, dateFilter, reloadHomePage, user, isLoggedIn } = useSelector((state) => state.user);
   const [crushes, setCrushes] = useState([]);
   const [page, setPage] = useState(1);
+  const [lastPage, setPastPage] = useState(null);
+  const [openLoading, setOpenLoading] = useState(true);
   const [loading, setLoading] = useState(false);
   const [subscriptionData, setSubscriptionData] = useState({});
   const [reinitList, setReinitList] = useState(false);
@@ -28,7 +30,7 @@ export default function WishList({ savedScroll, onSaveScroll }) {
 
   const onScroll = (e) => {
       onSaveScroll(window.scrollY);
-      console.log('onScroll', window.scrollY);
+      //console.log('onScroll', window.scrollY);
   };
 
   useEffect(() => {
@@ -56,22 +58,22 @@ export default function WishList({ savedScroll, onSaveScroll }) {
   }, [onScroll]);
   
   // fetch users from API
-  const fetchCrushes = async () => {
-      console.log('fetchCrushes', page)
+  const fetchCrushes = async (pageNum) => {
+      console.log('fetchCrushes', page, pageNum, crushes)
       setLoading(true);
+      //alert(page)
       try {
-          const res = await api.get(`get-wish-list?page=${page}`, {params: {}});
+          const res = await api.get(`get-wish-list?page=${pageNum}`, {params: {}});
           //const res = await api.get(`https://testnet-backend.piketplace.com/api/v1/index-loading?page=${page}`);
           setCrushes([...crushes, ...res.data.list.data]); // adjust to your API structure
-          if (res.data.list.data.length==0) {
-              setPage((p) => 1);
-          }
+          setPastPage(res.data.list.last_page)
           window.scrollTo(0, savedScroll || 0);
       } catch (err) {
           console.error("Error fetching users:", err);
       }
       console.log('crushes', crushes)
       setLoading(false);
+      setOpenLoading(false);
   };
   const redirectionTo = async (reaction) => {
       if (subscriptionData['see who likes you'] === true) {
@@ -80,23 +82,26 @@ export default function WishList({ savedScroll, onSaveScroll }) {
   };
   const reload = async () => {
       setReinitList(true)
+      setCrushes([])
   };
   
-  // Load data on page change
+  // Fetch first page on mount
   useEffect(() => {
     //alert('refetch '+page)
-    fetchCrushes();
-  }, [page]);
-  // Load data on page change
+    fetchCrushes(page);
+  }, []);
+  // Load data on crushes array change
   useEffect(() => {
-    //alert('reiniti')
+    //console.log('new value for crushes', crushes, page)
     if (reinitList) {
       setReinitList(false)
-      setCrushes([])
-      setPage(1);
-      fetchCrushes()
+      page!==1?setPage((p) => 1):fetchCrushes(1)
     }
-  }, [reinitList]);
+  }, [crushes]);
+  // Load data on crushes array change
+  useEffect(() => {
+    fetchCrushes(page)
+  }, [page]);
 
   useEffect(() => {
     const options = {
@@ -106,23 +111,24 @@ export default function WishList({ savedScroll, onSaveScroll }) {
     };
 
     const bottomObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          console.log("🔽 Bottom visible → load");
-          if (loading) return
-          setPage((p) => p + 1);
-        }
-      });
-    }, options);
+      console.log('gffffffffffffffffffff')
+      if (entries[0].isIntersecting && !loading && lastPage>page) {
+        console.log("🔽 Bottom visible → load", page);
+        setPage((p) => p + 1);
+      }
+    }, {threshold: 1});
 
     if (bottomRef.current) bottomObserver.observe(bottomRef.current);
 
     return () => {
-      bottomObserver.disconnect();
-    };
-  }, []);
+      if (bottomRef.current) {
+        bottomObserver.unobserve(bottomRef.current);
+      }
 
-  if (loading) {
+    };
+  }, [loading]);
+
+  if (openLoading) {
       return (
           <>
               <Header showBackButton={true} title={"Liked you"} showWishList={false}/>
@@ -151,11 +157,11 @@ export default function WishList({ savedScroll, onSaveScroll }) {
             </button>
           </div>
           <div className="row g-2" id="scroll-container"
-           ref={containerRef}
+            ref={containerRef}
           >
             {crushes.map((reaction, index) => (
-                <div key={reaction.id} className="col-6" onClick={() => redirectionTo(reaction)}>
-                  <div className="dz-media-card">
+                <div key={`reaction${reaction.id}-${index}`} className="col-6" onClick={() => redirectionTo(reaction)}>
+                  <div className="dz-media-card" style={{position: "relative"}}>
                     <a>
                       <div className="dz-media">
                         <img src={reaction.sender?.imageFirst} alt={reaction.sender?.firstname} style={{filter: `blur(${!subscriptionData['see who likes you']?20:0}px)`}}/>
@@ -165,12 +171,20 @@ export default function WishList({ savedScroll, onSaveScroll }) {
                         <span className="about" style={{filter: `blur(${!subscriptionData['see who likes you']?4:0}px)`}}>{reaction.sender?.about_me}</span> 
                       </div>
                     </a>
+                    <div style={{position: "absolute", top: "40%", width: "100%", padding: "20px"}}>
+                    <button onClick={() => navigate('/subscription')} className="btn btn-gradient w-100 btn-shadow rounded-xl my-2">
+                      {t('subscribe')}
+                    </button>
+                    </div>
                   </div>
                 </div>
               ))
             }
             {loading && (<div style={{width: "100%", textAlign: "center"}}>
-              <svg onClick={() => setPage(1)} className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent", margin: "auto"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
+              <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent", margin: "auto"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
+            </div>)}
+            {!loading && lastPage && lastPage<=page && (<div style={{width: "100%", textAlign: "center"}}>
+              No more elements
             </div>)}
             {/* bottom sentinel */}
             <div ref={bottomRef} style={{ height: 1 }} />
