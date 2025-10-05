59534ebd664c003d679e6ff2ed3ab5f3ec493544
Djedje SEWOVON <jesssewovon@gmail.com>
2025-10-05 15:13:00 +0000

    Updates

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -70,15 +70,17 @@ export default function Subscription() {
   if (isLoading) {
       return (
           <>
-              <Header showBackButton={true} title={"Subscriptions"} showWishList={false}/>
-              <div className="page-content space-top p-b65">
-                  <div className="container fixed-full-area">
-                      <div className="flex items-center justify-center h-screen bg-gray-100">
-                          <div className="" style={{width: "100%", height: "70vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                              <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
-                          </div>
-                      </div>
-                  </div>
+              <Header showBackButton={true} title={"Subscriptions"} showWishList={false} classes={`bg-gray-color`} />
+              <div className="content-body bg-gray-color" style={{height: "100vh"}}>
+                <div className="page-content space-top p-b65">
+                    <div className="container fixed-full-area">
+                        <div className="flex items-center justify-center h-screen bg-gray-100">
+                            <div className="" style={{width: "100%", height: "70vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                                <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </div>
           </>
       );
@@ -86,71 +88,73 @@ export default function Subscription() {
 
   return (
     <>
-      <Header showBackButton={true} title={"Subscriptions"} showWishList={false}/>
-      <div class="page-content space-top p-b60">
-        <div class="container"> 
-          <div class="dz-subscribe-area">
-            <div class="subscribe-area-top">
-              <div class="swiper subscription-swiper2">
-                <Swiper
-                    ref={swiperRef}
-                    onSlideChange={(swiper) => {
-                      //alert(swiper.activeIndex)
-                      setSubscriptionIndex(swiper.activeIndex)
-                    }}
-                    spaceBetween={30}
-                    speed={1500}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: true,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={false}
-                    modules={[Autoplay, Pagination, Navigation]}
-                    className="mySwiper get-started"
-                >
-                    {subscriptions?.map((subscription, index) => (
-                        <SwiperSlide key={`slider${subscription.id}`}>
-                            <div className={`subscribe-box ${index%2==0?'plus':'platinum'}`}>
-                              <h3 className="title">{subscription.name}</h3>
-                              {activeSubscription?.code==subscription.code && (<div className="badge">Active</div>)}
-                            </div>
-                        </SwiperSlide>
-                      ))
-                    }
-                </Swiper>
-                <div className="swiper-btn">
-                  <div className="swiper-pagination style-1 flex-1"></div>
+      <Header showBackButton={true} title={"Subscriptions"} showWishList={false} classes={`bg-gray-color`} />
+      <div className="content-body bg-gray-color" style={{height: "100vh"}}>
+        <div class="page-content space-top p-b60">
+          <div class="container"> 
+            <div class="dz-subscribe-area">
+              <div class="subscribe-area-top">
+                <div class="swiper subscription-swiper2">
+                  <Swiper
+                      ref={swiperRef}
+                      onSlideChange={(swiper) => {
+                        //alert(swiper.activeIndex)
+                        setSubscriptionIndex(swiper.activeIndex)
+                      }}
+                      spaceBetween={30}
+                      speed={1500}
+                      centeredSlides={true}
+                      autoplay={{
+                          delay: 3000,
+                          disableOnInteraction: true,
+                      }}
+                      pagination={{
+                          clickable: true,
+                      }}
+                      navigation={false}
+                      modules={[Autoplay, Pagination, Navigation]}
+                      className="mySwiper get-started"
+                  >
+                      {subscriptions?.map((subscription, index) => (
+                          <SwiperSlide key={`slider${subscription.id}`}>
+                              <div className={`subscribe-box ${index%2==0?'plus':'platinum'}`}>
+                                <h3 className="title">{subscription.name}</h3>
+                                {activeSubscription?.code==subscription.code && (<div className="badge">Active</div>)}
+                              </div>
+                          </SwiperSlide>
+                        ))
+                      }
+                  </Swiper>
+                  <div className="swiper-btn">
+                    <div className="swiper-pagination style-1 flex-1"></div>
+                  </div>
                 </div>
               </div>
+              {subscriptions?.map((subscription, index) => (
+                  subscriptionIndex === index && (<div key={subscription.id} className={`subscribe-content ${index%2==0?'plus':'platinum'}`}>
+                    <ul className="pricing-data">
+                      {
+                        subscription?.contents?.map((content, index1) => (
+                          <li className="list-true">
+                            {((content.item.type=='boolean' && content.value==1)) && (<i className="icon feather icon-check"></i>)}
+                            {((content.item.type=='boolean' && content.value==0) || (content.item.type=='int' && content.value==0)) && (<i className="icon feather icon-lock"></i>)}
+                            {(content.item.type=='int' && content.value!=0 && content.value!=-1) && (<i>{content.value}</i>)}
+                            <span>{content.name}</span>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                    {activeSubscription?.code!==subscription.code && (<div className="bottom-btn container bg-white text-center px-5">
+                      <Link to={`/subscription-details/${subscription.id}`} className="btn btn-gradient dz-flex-box btn-shadow rounded-xl">
+                      {t('subscribe')}
+                      </Link>
+                    </div>)}
+                  </div>)
+                ))
+              }
             </div>
-            {subscriptions?.map((subscription, index) => (
-                subscriptionIndex === index && (<div key={subscription.id} className={`subscribe-content ${index%2==0?'plus':'platinum'}`}>
-                  <ul className="pricing-data">
-                    {
-                      subscription?.contents?.map((content, index1) => (
-                        <li className="list-true">
-                          {((content.item.type=='boolean' && content.value==1)) && (<i className="icon feather icon-check"></i>)}
-                          {((content.item.type=='boolean' && content.value==0) || (content.item.type=='int' && content.value==0)) && (<i className="icon feather icon-lock"></i>)}
-                          {(content.item.type=='int' && content.value!=0 && content.value!=-1) && (<i>{content.value}</i>)}
-                          <span>{content.name}</span>
-                        </li>
-                      ))
-                    }
-                  </ul>
-                  {activeSubscription?.code!==subscription.code && (<div className="bottom-btn container bg-white text-center px-5">
-                    <Link to={`/subscription-details/${subscription.id}`} className="btn btn-gradient dz-flex-box btn-shadow rounded-xl">
-                     {t('subscribe')}
-                    </Link>
-                  </div>)}
-                </div>)
-              ))
-            }
-          </div>
-        </div> 
+          </div> 
+        </div>
       </div>
     </>
   );
--- src/pages/SubscriptionDetails.jsx
+++ src/pages/SubscriptionDetails.jsx
@@ -75,15 +75,17 @@ export default function SubscriptionDetails() {
   if (isLoading) {
       return (
           <>
-              <Header showBackButton={true} title={"Subscription details"} showWishList={false}/>
-              <div className="page-content space-top p-b65">
-                  <div className="container fixed-full-area">
-                      <div className="flex items-center justify-center h-screen bg-gray-100">
-                          <div className="" style={{width: "100%", height: "70vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                              <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
-                          </div>
-                      </div>
-                  </div>
+              <Header showBackButton={true} title={"Subscriptions"} showWishList={false} classes={`bg-gray-color`} />
+              <div className="content-body bg-gray-color" style={{height: "100vh"}}>
+                <div className="page-content space-top p-b65">
+                    <div className="container fixed-full-area">
+                        <div className="flex items-center justify-center h-screen bg-gray-100">
+                            <div className="" style={{width: "100%", height: "70vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                                <svg className="loader-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{shaperendering: "auto", display: "block", background: "transparent"}} width="50" height="50" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50"><animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform></circle><g></g></g></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </div>
           </>
       );
@@ -91,122 +93,124 @@ export default function SubscriptionDetails() {
 
   return (
     <>
-      <Header showBackButton={true} title={"Subscription details"} showWishList={false}/>
-      <div class="page-content space-top p-b60">
-        <div class="container"> 
-          <div class="dz-subscribe-area">
-            <div class="subscribe-area-top">
-              <div class="swiper subscription-swiper2">
-                <div className={`subscribe-box`}>
-                  <h3 className="title">{subscription?.name}</h3>
-                  {isActiveSubscription && (<div className="badge" style={{color: '#fb7c67'}}>Active</div>)}
-                </div>
-                <div className="swiper-btn">
-                  <div className="swiper-pagination style-1 flex-1"></div>
+      <Header showBackButton={true} title={"Subscriptions"} showWishList={false} classes={`bg-gray-color`} />
+      <div className="content-body bg-gray-color" style={{height: "100vh"}}>
+        <div class="page-content space-top p-b60">
+          <div class="container"> 
+            <div class="dz-subscribe-area">
+              <div class="subscribe-area-top">
+                <div class="swiper subscription-swiper2">
+                  <div className={`subscribe-box`}>
+                    <h3 className="title">{subscription?.name}</h3>
+                    {isActiveSubscription && (<div className="badge" style={{color: '#fb7c67'}}>Active</div>)}
+                  </div>
+                  <div className="swiper-btn">
+                    <div className="swiper-pagination style-1 flex-1"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-            {!isActiveSubscription && (<div>
-              <div className="section-head ps-0 py-4">
-                  <h5>Choose a period</h5>
-              </div>
-              <div className="radio style-2">
+              {!isActiveSubscription && (<div>
+                <div className="section-head ps-0 py-4">
+                    <h5>Choose a period</h5>
+                </div>
+                <div className="radio style-2">
+                    {
+                      subscription?.periods?.map((period, index) => (
+                        <label key={index} className="radio-label" htmlFor={period.period_interval}>
+                            <input type="radio" name="period" value={period.id}
+                                id={period.period_interval} 
+                                checked={
+                                    period.id ===
+                                    selectedPeriod.id
+                                }
+                                onChange={() =>
+                                    handleRadioChange(
+                                        period
+                                    )
+                                }/>
+                            <span className="checkmark">						
+                                <span className="text">{period.nb_period} {period.period}</span>
+                                <span className="check"></span>							
+                            </span>
+                        </label>
+                      ))
+                    }
+                </div>
+              </div>)}
+              <div className={`subscribe-content`}>
+                <ul className="pricing-data">
                   {
-                    subscription?.periods?.map((period, index) => (
-                      <label key={index} className="radio-label" htmlFor={period.period_interval}>
-                          <input type="radio" name="period" value={period.id}
-                              id={period.period_interval} 
-                              checked={
-                                  period.id ===
-                                  selectedPeriod.id
-                              }
-                              onChange={() =>
-                                  handleRadioChange(
-                                      period
-                                  )
-                              }/>
-                          <span className="checkmark">						
-                              <span className="text">{period.nb_period} {period.period}</span>
-                              <span className="check"></span>							
-                          </span>
-                      </label>
+                    subscription?.contents?.map((content, index) => (
+                      <li key={index} className="list-true">
+                        {((content.item.type=='boolean' && content.value==1)) && (<i className="icon feather icon-check"></i>)}
+                        {((content.item.type=='boolean' && content.value==0) || (content.item.type=='int' && content.value==0)) && (<i className="icon feather icon-lock"></i>)}
+                        {(content.item.type=='int' && content.value!=0 && content.value!=-1) && (<i>{content.value}</i>)}
+                        <span>{content.name}</span>
+                      </li>
                     ))
                   }
+                  {/* <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Unlimited Likes</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>See Who Likes You</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>Priority Likes</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Unlimited Rewinds</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>1 Free Boost per month</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>5 Free Super Likes per week</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>Message Before Matching</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Passport</span>
+                  </li>
+                  <li class="list-false">
+                    <i class="icon feather icon-lock"></i>
+                    <span>Top Picks</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Control Your Profile</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Control Who Sees You</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Control Who You See</span>
+                  </li>
+                  <li class="list-true">
+                    <i class="icon feather icon-check"></i>
+                    <span>Hide Ads</span>
+                  </li> */}
+                </ul>
+                {!isActiveSubscription && (<div className="bottom-btn container bg-white text-center px-5">
+                  <button disabled={!selectedPeriod.id} onClick={() => makePayment()} className="btn btn-gradient dz-flex-box btn-shadow rounded-xl w-100">
+                    {selectedPeriod.amount} Pi {'=>'} {t('subscribe')}
+                  </button>
+                </div>)}
               </div>
-            </div>)}
-            <div className={`subscribe-content`}>
-              <ul className="pricing-data">
-                {
-                  subscription?.contents?.map((content, index) => (
-                    <li key={index} className="list-true">
-                      {((content.item.type=='boolean' && content.value==1)) && (<i className="icon feather icon-check"></i>)}
-                      {((content.item.type=='boolean' && content.value==0) || (content.item.type=='int' && content.value==0)) && (<i className="icon feather icon-lock"></i>)}
-                      {(content.item.type=='int' && content.value!=0 && content.value!=-1) && (<i>{content.value}</i>)}
-                      <span>{content.name}</span>
-                    </li>
-                  ))
-                }
-                {/* <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Unlimited Likes</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>See Who Likes You</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>Priority Likes</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Unlimited Rewinds</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>1 Free Boost per month</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>5 Free Super Likes per week</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>Message Before Matching</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Passport</span>
-                </li>
-                <li class="list-false">
-                  <i class="icon feather icon-lock"></i>
-                  <span>Top Picks</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Control Your Profile</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Control Who Sees You</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Control Who You See</span>
-                </li>
-                <li class="list-true">
-                  <i class="icon feather icon-check"></i>
-                  <span>Hide Ads</span>
-                </li> */}
-              </ul>
-              {!isActiveSubscription && (<div className="bottom-btn container bg-white text-center px-5">
-                <button disabled={!selectedPeriod.id} onClick={() => makePayment()} className="btn btn-gradient dz-flex-box btn-shadow rounded-xl w-100">
-                  {selectedPeriod.amount} Pi {'=>'} {t('subscribe')}
-                </button>
-              </div>)}
             </div>
-          </div>
-        </div> 
+          </div> 
+        </div>
       </div>
     </>
   );
